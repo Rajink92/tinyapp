ec8a0eaed6ddfab28dcc4fd15e7a837ae6723516
Rajin <rajink92@gmail.com>
2022-03-14 16:41:44 -0400

    Add urls new ejs, and random string

--- express_server.js
+++ express_server.js
@@ -4,10 +4,23 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const generateRandomString = () => {
+  const randomString = Math.random().toString(36).slice(-6);
+  return randomString;
+};
+
+console.log(generateRandomString(6));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,6 +39,11 @@ app.get("/hello", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");         
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
